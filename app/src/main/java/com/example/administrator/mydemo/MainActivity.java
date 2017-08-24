9d34a31ace0eb5a6c87983883390fb38e96f3c07
1043591427 <ththiecsobj@qq.com>
2017-08-24 09:46:21 +0800

    演示。。。

---
+++ app/src/main/java/com/example/administrator/mydemo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.administrator.mydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        getOnClicks();
+
+    }
+
+    private void getOnClicks() {
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "按钮点击测试1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "按钮点击测试2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "按钮点击测试3", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initViews() {
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+    }
+
+    public class Demo{
+        private String a = "";
+        private String b = "";
+        Demo() {
+        }
+
+        private boolean getDemo(){
+            if (a == null) {
+                a = "String";
+            }
+            return !a.isEmpty();
+        }
+
+
+    }
+
+}
